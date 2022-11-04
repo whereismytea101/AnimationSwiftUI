beea0ebc09d64c3c31bd6aa9046ecb058b5b0700
Samir <imnotajedi@icloud.com>
2022-11-04 19:09:17 +0600

    Initial Commit

---
+++ AnimationSwiftUI/AnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSwiftUIApp.swift
+//  AnimationSwiftUI
+//
+//  Created by Самир Кафаров on 04.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
